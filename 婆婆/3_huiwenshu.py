46a425a3982d07fcca0b2f3ab09c377ea95bf0dd
lsynaturelover <liusiying1012@163.com>
2019-04-27 11:30:38 -0700

    Update 3_huiwenshu.py
    add

--- 婆婆/3_huiwenshu.py
+++ 婆婆/3_huiwenshu.py
@@ -4,7 +4,6 @@ Created on Sat Apr 27 10:37:47 2019
 
 @author: liusi
 """
-import copy
 
 #approach 1
 def isPalindrome(x):
@@ -20,23 +19,17 @@ def isPalindrome(x):
 def isPalindrome1(x):
     if x < 0:
         return False
-    elif x == 0:
+    li = []
+    y = x
+    while y >= 1:
+        a = y % 10
+        li.append(a)
+        y //= 10
+    z = 0
+    for index, value in enumerate(li):
+        z += value*10**(len(li)-index-1)
+    if z == x:
         return True
     else:
-        if x % 10 == 0:
-            return False
-        else:
-            li = []
-            y = copy.copy(x)
-            while y >= 1:
-                a = y % 10
-                li.append(a)
-                y //= 10
-            z = 0
-            for index, value in enumerate(li):
-                z += value*10**(len(li)-index-1)
-            if z == x:
-                return True
-            else:
-                return False
+        return False
             
